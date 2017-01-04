2218ae1b347bd37b7632ec2dd4c4b8e71dafebb9
ketjow4 <wojtek.adam.dudzik@gmail.com>
2017-01-04 21:10:27 +0100

    Created empty project in Visual Studio with description of work

---
+++ AKD_Project/main.cpp
@@ -0,0 +1,33 @@
+﻿//TODO
+//Temat: Klasa C++ reprezentująca archiwum ZIP
+//	Liczba osób : 2
+//	Standard formatu ZIP dopuszcza dwa algorytmy kompresji, które są implementowane
+//	za pomocą poznanych bibliotek : zlib oraz libbzip2(patrz pierwsze laboratorium).
+//	Tematem projektu jest wykorzystanie obu tych bibliotek do stworzenia klasy
+//	języka C++ reprezentującej archiwum ZIP.
+//	Klasa powinna udostępniać min.następujące metody :
+//	   • Create - utworzenie pustego archiwum,
+//		   • Open - otwarcie archiwum,
+//		   • Close - zamknięcie archiwum,
+//		   • ListContents - zwrócenie listy plików(do listy),
+//		   • GetHeader - zwrócenie nagłówka konkretnego pliku z archiwum,
+//		   • AddFile - dodanie pliku do archiwum z kompresją(tu się ustala parametry kompresji),
+//		   • AddFileRaw - dodanie do archiwum pliku skompresowanego(przydaje się kiedy
+//			   trzeba przenosić plik z archiwum do archiwum - np.przy wyrzucaniu niektó -
+//			   rych plików, bądź zastępowaniu nową wersją),
+//		   • GetFile - dekompresja pliku z archiwum,
+//		   • GetFileRaw - zwrócenie z archiwum pliku skompresowanego.
+//		   Klasa powinna także umożliwiać podpięcie przez użytkownika własnego paska postępu
+//		   (np.poprzez odziedziczenie po jakiejś klasie bazowej) i własnej obsługi błędów.
+
+
+
+using namespace std;
+
+
+
+
+int main(int ac, char* av[])
+{
+	return 0;
+}
